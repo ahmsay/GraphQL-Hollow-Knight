413355791d4ef9541ee30553a66eb481f5622b81
Ahmet Say <ahmsay@gmail.com>
2019-02-15 16:55:43 +0300

    trying to deploy heroku

--- server/app.js
+++ server/app.js
@@ -1,21 +1,29 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+/*const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');*/
 
 const app = express();
 
-mongoose.connect('mongodb+srv://paleKing:theWhiteLady@hollowknightgraphql-doplt.mongodb.net/HollowKnight?retryWrites=true', { useNewUrlParser: true });
+/*mongoose.connect('mongodb+srv://paleKing:theWhiteLady@hollowknightgraphql-doplt.mongodb.net/HollowKnight?retryWrites=true', { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
   console.log('Connected to database.');
 });
-mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false);*/
 
-app.use('/graphql', graphqlHTTP({
+app.use(express.static('public'));
+
+/*app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
-}));
+}));*/
 
-app.listen(4000, () => {
-  console.log('App running on port 4000...');
+const port = process.env.PORT || 4000;
+
+/*app.get('/', function (req, res) {
+  res.send("<div>Click <a href='http://localhost:" + port + "/graphql'>here</a> to use the API.</div>");
+});*/
+
+app.listen(port, () => {
+  console.log('App running on port ' + port + '...');
 });
