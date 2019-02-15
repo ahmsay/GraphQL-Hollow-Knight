5cdc2fb0950528db1f4ad7e3a292fdc5f9ddf77c
Ahmet Say <ahmsay@gmail.com>
2019-02-16 01:21:52 +0300

    all set

--- server/app.js
+++ server/app.js
@@ -18,9 +18,15 @@ app.use('/graphql', graphqlHTTP({
 
 const port = process.env.PORT || 4000;
 
+let url = '';
+if(port == 4000)
+  url = 'http://localhost:4000/graphql';
+else
+  url = 'https://hollow-knight-gql.herokuapp.com/graphql';
+
 app.get('/', function (req, res) {
   let content = "<h1>Hollow Knight GraphQL API</h1>" +
-  "<span>Click <a href='http://localhost:" + port + "/graphql'>here</a> to use the API.</span>";
+  "<span>Click <a href='" + url + "'>here</a> to use the API.</span>";
   res.send(content);
 });
 
